1d551d58d5ceb8cef6aa8ad55c07418f17c3ca32
itudor13 <ian@decaturinvestments.com>
2026-09-24 20:20:46 -0700

    Expand inline calendar view

--- app/how-it-works/page.js
+++ app/how-it-works/page.js
@@ -75,7 +75,7 @@ function CalendlyEmbed() {
       <div
         className="calendly-inline-widget"
         data-url={CALENDLY_URL}
-        style={{ minWidth: "320px", height: "700px" }}
+        style={{ minWidth: "320px", height: "1000px" }}
       />
     </div>
   );
