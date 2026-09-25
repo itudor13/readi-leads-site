3710bb3b748643d0b354a5ebccfff392328b84de
itudor13 <ian@decaturinvestments.com>
2026-09-24 20:22:54 -0700

    Expand main page booking calendars

--- app/page.js
+++ app/page.js
@@ -192,7 +192,7 @@ function CalendlyEmbed({ id }) {
       <div
         className="calendly-inline-widget"
         data-url={CALENDLY_URL}
-        style={{ minWidth: "320px", height: "700px" }}
+        style={{ minWidth: "320px", height: "1000px" }}
       />
     </div>
   );
